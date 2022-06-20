07605e212f1cef225a71b88e7d6bb66521e104ae
tatiniG <65956745+tatiniG@users.noreply.github.com>
2022-06-20 12:21:34 +0530

    Add files via upload

---
+++ addstudent.php
@@ -0,0 +1,261 @@
+<?php 
+require 'includes/snippet.php';
+    require 'includes/db-inc.php';
+include "includes/header.php"; 
+$nameErr= $pwdErr= $cpwdErr= $contactErr = $mailErr= $mErr= $userErr="";
+if(isset($_POST['submit'])) {
+
+    $matric = sanitize(trim($_POST['matric_no']));
+    $password = sanitize(trim($_POST['password']));
+    $password2 = sanitize(trim($_POST['password2']));
+    $username = sanitize(trim($_POST['username']));
+    $email = sanitize(trim($_POST['email']));
+    $dept = sanitize(trim($_POST['dept']));
+    $books = sanitize(trim($_POST['num_books']));
+    $money = sanitize(trim($_POST['money_owed']));
+    $phone = sanitize(trim($_POST['phone']));
+    $name = sanitize(trim($_POST['name']));
+    $filename =''; 
+
+if (isset($_FILES['postimg'])) {
+        $img_size = $_FILES['postimg']['size'];
+        $temp = $_FILES['postimg']['tmp_name'];
+        $img_type = $_FILES['postimg']['type'];
+        $img_name = $_FILES['postimg']['name'];
+
+        if(empty($_POST["name"])){
+            $valid=FALSE;
+        $nameErr="NAME IS REQUIRED";
+    }
+    else{
+        $name=$_POST["name"];
+        if (!preg_match ("/^[a-zA-z]*$/", $name) ) {  
+            $valid=FALSE;
+            $nameErr= "Only alphabets  digits are not allowed.";  
+        }
+    }
+    if(empty($_POST['username'])){
+        $valid=FALSE;
+        $userErr="username must be filled";
+    }
+    else{
+        $username=$_POST['username'];
+        if(strlen ($username)== 9){
+            $valid=FALSE;
+            $userErr="username must contain atleast 8 characters.";
+        }
+    }
+    if(empty($_POST["email"])){
+        $valid=FALSE;
+        $mailErr="MAIL IS REQUIRED";
+    }else{
+        $email=$_POST["email"];
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { 
+            $valid=FALSE; 
+            $mailErr = "Invalid email format";  
+        }  
+    }
+    if(empty($_POST['password'])){
+       
+        $valid=FALSE;
+        $pwdErr="password must be filled";
+    }
+    else{
+        $password=$_POST['password'];
+       
+          $uppercase = preg_match('@[A-Z]@', $password);
+          $lowercase = preg_match('@[a-z]@', $password);
+          $number    = preg_match('@[0-9]@', $password);
+          $specialChars = preg_match('@[^\w]@', $password);
+        if(!$uppercase || !$lowercase || !$number || !$specialChars || (strlen($password) > 6 && strlen($password) <= 10)){
+            $valid=FALSE;
+            $pwdErr="password must contain atleast 6 characters.";
+        }
+    }
+    if(empty($_POST['password2'])){
+        $valid=FALSE;
+        $cpwdErr="C password must be filled";
+    }
+    else{
+        $password2=$_POST['password2'];
+        if($password2 != $password){
+            $valid=FALSE;
+            $cpwdErr="must match with password ,Re-enter the c password again.";
+        }
+    }
+    if(empty($_POST["phone"])){
+        $valid=FALSE;
+        $contactErr="CONTACT NO IS REQUIRED";
+    }else{
+        $phone=$_POST["phone"];
+        if (!preg_match ("/^[0-9]*$/", $phone) ) {  
+            $valid=FALSE;
+            $contactErr = "Only numeric value is allowed.";  
+            }  
+        //check mobile no length should not be less and greator than 10  
+        if (strlen ($phone) != 10) {  
+            $valid=FALSE;
+            $contactErr = "Mobile no must contain 10 digits.";  
+            }  
+    }
+
+        if ($img_size > 500000) {
+            $err_flag = true;
+            $error = "Your image size is too big... Try again.";
+        }
+
+        $extensions = array('jpg', 'jpeg', 'png', 'gif');
+        $img_ext = explode('/', $img_type);
+        $img_ext = end($img_ext);
+        $img_ext = strtolower($img_ext);
+        if (!(in_array($img_ext, $extensions))) {
+            $err_flag = true;
+            $error = "Unwanted image file type... Only jpg,jpeg,png and gif allowed";
+        }
+        // Prepare image folder in the server
+        $imgFile = 'posts-images/';
+        $filename = rand(1000, 8000) . '_' .time() . '.' . $img_ext;
+        $filepath = $imgFile . $filename;
+        // if (isset($err_flag) && $err_flag === false) {
+        //     $result = move_uploaded_file($temp, $filepath);
+        //     if ($result) {
+        //         $magicianObj = new imageLib($filepath);
+        //         $magicianObj -> resizeImage(100, 100);
+        //         $magicianObj -> saveImage($imgFile . 'thumbnails/' . $filename, 100);
+        //         $img_path = $imgFile.$img_name;
+        //     }
+        // }
+    }
+
+    // echo $filename;
+
+   if ($password==$password2) {
+      $sql = "INSERT INTO students( matric_no, password, username, email, dept, numOfBooks, moneyOwed, photo, phoneNumber, name)
+ VALUES ('$matric', '$password', '$username', '$email', '$dept', '$books', '$money', '$filename', '$phone', '$name' ) ";
+
+      $query = mysqli_query($conn, $sql);
+      $error = false;
+      if($query){
+       $error = true;
+      }
+      else{
+        echo "<script>alert('Not Registered successful!! Try again.');
+                    </script>"; 
+      }
+   }
+   else {
+    echo  "<script>alert('Password mismatched!')</script>";
+   }
+    
+
+}
+
+?>
+
+
+<div class="container">
+    <?php include "includes/nav.php"; ?>
+    
+    <div class="container  col-lg-9 col-md-11 col-sm-12 col-xs-12 col-lg-offset-2 col-md-offset-1 col-sm-offset-0 col-xs-offset-0  " style="margin-top: 20px">
+        <div class="jumbotron login3 col-lg-10 col-md-11 col-sm-12 col-xs-12">
+
+              <?php if(isset($error)===true) { ?>
+        <div class="alert alert-success alert-dismissable">
+                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                  <strong>Record Added Successfully!</strong>
+            </div>
+            <?php } ?>
+        
+            <p class="page-header" style="text-align: center">ADD STUDENTS</p>
+
+            <div class="container">
+                <form class="form-horizontal" role="form" action="addstudent.php" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="Username" class="col-sm-2 control-label">FULL NAME</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="name" placeholder="Full name" id="name" ><span id="err"><?php echo $nameErr; ?></span><br>
+                        </div>      
+                    </div>
+                    <div class="form-group">
+                        <label for="Password" class="col-sm-2 control-label">Reg No</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="matric_no" placeholder="Registration number" id="password" ><span id="err"><?php echo $mErr; ?></span><br>
+                        </div>      
+                    </div>
+                    <div class="form-group">
+                        <label for="Password" class="col-sm-2 control-label">DEPT</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="dept" placeholder="Department" id="Address" >
+                        </div>      
+                    </div>
+                    <div class="form-group">
+                        <label for="Password" class="col-sm-2 control-label">EMAIL</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" name="email" placeholder="Email" id="password" ><span id="err"><?php echo $mailErr; ?></span><br>
+                        </div>      
+                    </div>
+                    <div class="form-group">
+                        <label for="Password" class="col-sm-2 control-label">USERNAME</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="username" placeholder="Username" id="password" ><span id="err"><?php echo $userErr; ?></span><br>
+                        </div>      
+                    </div>
+                    <div class="form-group">
+                        <label for="Password" class="col-sm-2 control-label">PASSWORD</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" name="password" placeholder="password" id="password" ><span id="err"><?php echo $pwdErr; ?></span><br>
+                        </div>      
+                    </div>
+                    <div class="form-group">
+                        <label for="Password" class="col-sm-2 control-label">CONFRIM PASSWORD</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" name="password2" placeholder="Confirm password" id="password" ><span id="err"><?php echo $cpwdErr; ?></span><br>
+                        </div>      
+                    </div>
+                     
+                     <input type="hidden" class="form-control" name="num_books" placeholder="books" id="password" required value="null">
+                     <input type="hidden" class="form-control" name="money_owed" placeholder="Money" id="password" required value="null">
+                    <div class="form-group">
+                        <label for="Password" class="col-sm-2 control-label">PHONE NUMBER</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="phone" placeholder="phone" id="password" ><span id="err"><?php echo $contactErr; ?></span><br>
+                        </div>      
+                        </div>      
+                    </div>     
+                         
+                    
+                     <div class="form-group">
+                        <label class="col-sm-2 control-label">IMAGE</label>
+                        <div class="col-sm-10">
+                            <input type="file" class="form-control" name="postimg" placeholder="Enter image" id="password" style="padding: 0" required>
+                        </div>      
+                    </div>
+                    
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button  class="btn btn-info col-lg-12" data-toggle="modal" data-target="#info" name="submit">
+                                ADD MEMBER
+                            </button>
+                            
+                        </div>
+                    </div>
+
+                    
+                </form>
+            </div>
+        </div>
+        
+    </div>
+</div>  
+
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript">
+ 	window.onload = function () {
+		var input = document.getElementById('name').focus();
+	}
+ </script>
+</body>
+</html>
---
+++ studentportal.php
@@ -0,0 +1,149 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="flickity/flickity.css">
+	<script type="text/javascript" src="flickity/flickity.js"></script>
+	<title>Library Management</title>
+
+</head>
+<body>
+<div class="container">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example">
+					<span class="sr-only">:</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">VIT-AP Library Management System</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="profile.php">View Profile</a></li>
+					<li><a href="borrow-student.php">Borrow Books</a></li>					
+					<li><a href="fine-student.php">Fines</a></li>					
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+</div>
+
+		<div class="container-fluid slide">
+			
+	  		<div class="slider">
+	  			<!-- <h1>Flickity - wrapAround</h1> -->
+
+
+					<div class="carousel" data-flickity='{ "autoPlay": true }'; >
+
+						  <div class="carousel-cell" auto-play >
+						  	<img src="images/header-bg.jpg">
+						  </div>
+						  <div class="carousel-cell" auto-play>
+						  	<img src="images/header-bg.jpg">
+
+						  </div>
+						  <div class="carousel-cell" auto-play>
+						  	 <img src="images/header-bg.jpg">
+						  </div>
+						  
+						  <div class="carousel-cell" auto-play >
+						  	<img src="images/header-bg.jpg">
+						  </div>
+						   <div class="carousel-cell" auto-play>
+						  	<img src="images/header-bg.jpg">
+						  </div>
+
+					</div>
+
+					
+
+	  		</div>
+		</div>
+
+		<div class="container slide2">
+			
+	  		<div class="row">
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+		  			<div class="page-header col-lg-offset-1">
+		  				<h2>Welcome to our portal</h2>
+		  			</div>
+	  				With a history of 37 years of innovation in educational and research domain, VIT  has been a forerunner in delivering quality education. Consistently ranked among the top educational institutes in the country, the VIT group of institutions have had a proud tradition of pursuing knowledge and excellence. In keeping with this tradition,  the leadership at VIT-AP resonates a dynamic blend of academic initiative and industry partnership with a vision of creating one of the finest academic destinations in the world.  The VIT-AP campus, which is poised to become one of the country’s best campuses, offers several avenues to explore your interests, identify core competencies, and engage in an evolving lifecycle of education and growth.
+
+	  				<a href="#"><p class="slide2"><button class="btn btn-success">Sign Up</button></p></a>
+	  			</div>
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+		  			<div class="page-header col-lg-offset-1">
+		  				<h2>24/7 Operational Support</h2>
+		  			</div>
+	  				A unique mentoring programme is in place at our institute, where, a faculty member is assigned to a group of students as a mentor. The mentor not only helps a student in making informed decisions about his or her academic progress, but also acts as a local guardian.
+					  We have highly-qualified, experienced, committed and caring faculty to help students adjust to living and studying at VIT – AP. Our faculty members are well abreast with the current trends in Education and Technology and impart the same in classrooms and laboratories. 
+	  			</div>
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+	  				<div class="page-header col-lg-offset-1">
+	  				<h2>Why Us?</h2>
+	  			</div>
+				  Seize the Unbeatable Placement Advantage [700+ companies, multiple offers]. At VIT-AP, students are part of serious and rigorous programmes which do not compromise on quality. As a result, they receive unparalleled placement opportunities through the well-connected placement cell.
+International tie-ups have already resulted in several interactions with faculty from abroad such as Rochester Institute of Technology (USA) and Queensland University of Technology (Australia). Our international programmes are aimed at providing a global experience to the students with our SAP (Study Abroad Programmes) and ITP (International Transfer Programme-2 years abroad and 2 years at VIT-AP)
+Collaborative Masters + Doctoral level studies with international universities
+	  			</div>
+	  		</div>
+		</div>
+
+		<div class="container-fluid slide3" style="background-color: #282828">
+			<div class="container">
+				<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="images/thumbnail.jpg">
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="images/thumbnail.jpg">
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="images/thumbnail.jpg">
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="images/thumbnail.jpg">
+					</a>
+				</div>
+			</div>
+			</div>
+			
+		</div>
+		<div class="container_fluid">
+			<p class="page-header center-block">
+				VIT-AP, Apply Knowledge. Improve Life!
+			</p>
+			<p class="page-header center-block">
+				<a href="#"><button class="btn btn-primary">Contact Us</button></a>
+				<a href="#"><button class="btn btn-info">About Us</button></a>
+
+			</p>
+		</div>
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
